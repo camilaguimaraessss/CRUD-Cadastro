a5b2c5ea63550e96b290af85ca370802ef3a97e5
Lucas Borges Moraes do Nascimento <m1r4sem@gmail.com>
2025-08-25 17:40:11 -0300

    Update server.js

--- public/server.js
+++ public/server.js
@@ -2,7 +2,9 @@ const express = require('express');
 const mysql = require('mysql2');
 const bodyParser = require('body-parser');
 const path = require('path');
+
 const app = express();
+const PORT = 3001;
 
 app.use (bodyParser.urlencoded({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
@@ -11,34 +13,32 @@ app.use(express.static(path.join(__dirname, 'public')));
 const db = mysql.createConnection({ 
     host: 'localhost',
     user: 'root',
-    password: '',
     database: 'crud',
     port: '3309'
 });
 
 app.get('/', (req, res) =>{
-    res.sendFile(process.cwd() + "/index.html")
+    res.sendFile(path.join(__dirname, 'index.html'));
 })
 
 app.post('/cadastrar', (req, res) => {
-    const { nome, email, telefone} = req.body;
+
+    const { nome, email, telefone } = req.body;
 
     const sql = "INSERT INTO usuarios (nome, email, telefone) VALUES (?, ?, ?)";
+
     db.query(sql, [nome, email, telefone], (err, result) => {
         if(err) throw err;
         res.send("Usuário cadastrado com sucesso!");
     });
 });
 
-app.get('/deletar', (req, res) =>{
-    res.sendFile(process.cwd() + "/delete.html")
-})
-
-
 app.post('/deletar', (req, res) => {
+
     const{ nome } = req.body;
 
     const sql = "DELETE FROM usuarios WHERE nome = ?";
+
     db.query(sql, [nome], (err, result) =>{
         if(err) throw err;
         if (result.affectedRows > 0) {
@@ -47,8 +47,9 @@ app.post('/deletar', (req, res) => {
             res.send(`Nenhum usuário encontrado com o nome ${nome}.`);
         }
     });
+    res.redirect('/');
 });
 
-app.listen(3010, () => {
-    console.log(`O servidor está ativo em http://localhost:3010`);
+app.listen(PORT, () => {
+    console.log(`O servidor está ativo em http://localhost:${PORT}`);
 });
